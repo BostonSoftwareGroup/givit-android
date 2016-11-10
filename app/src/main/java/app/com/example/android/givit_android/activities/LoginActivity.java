9efb646c950736dfa3a347b354e761d2fda57961
Mubing Liu <mubingliu@Mubings-MacBook-Pro.local>
2016-11-10 14:07:01 -0500

    Moved login block in activity to fragment.

---
+++ app/src/main/java/app/com/example/android/givit_android/activities/LoginActivity.java
@@ -0,0 +1,33 @@
+package app.com.example.android.givit_android.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import app.com.example.android.givit_android.R;
+import app.com.example.android.givit_android.fragments.LoginFragment;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        replaceFragment(R.id.login_fragment, new LoginFragment(), true);
+    }
+
+    private void replaceFragment(int layout, Fragment newFragment, boolean isFirstFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isFirstFragment) {
+            fragmentTransaction.add(layout, newFragment);
+        } else {
+            fragmentTransaction.replace(layout, newFragment);
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
+    }
+}
