e341aafcf21bfab75984bf6bda6c8cb304477760
Mubing Liu <mubingliu@Mubings-MacBook-Pro.local>
2016-11-22 22:36:56 -0500

    Modified login and signup fragment layout

--- app/src/main/java/app/com/example/android/givit_android/fragments/LoginFragment.java
+++ app/src/main/java/app/com/example/android/givit_android/fragments/LoginFragment.java
@@ -9,6 +9,7 @@ import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import app.com.example.android.givit_android.R;
@@ -19,8 +20,16 @@ import butterknife.OnClick;
 public class LoginFragment extends Fragment {
     @BindView(R.id.password)
     EditText passwordEditText;
+    @BindView(R.id.long_button)
+    Button longButton;
+    @BindView(R.id.signUpButton)
+    Button signUpButton;
+    @BindView(R.id.logInButton)
+    Button logInButton;
 
     private ForgotPasswordListener forgotPasswordListener;
+
+    protected View rootView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        this.rootView = view;
         ButterKnife.bind(this, view);
         return view;
     }
@@ -39,17 +49,52 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setPasswordHintFont();
+        initSignUpButtonLayout();
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        forgotPasswordListener = (ForgotPasswordListener)getContext();
+        forgotPasswordListener = (ForgotPasswordListener) getContext();
     }
 
     private void setPasswordHintFont() {
         passwordEditText.setTypeface(Typeface.DEFAULT);
         passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+    }
+
+    private void initSignUpButtonLayout() {
+        signUpButton.setBackgroundColor(getResources().getColor(R.color.colorMainRed));
+        signUpButton.setTextColor(getResources().getColor(R.color.colorWhite));
+
+        logInButton.setTextColor(getResources().getColor(R.color.colorMainRed));
+        logInButton.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+
+        longButton.setBackgroundResource(R.drawable.red_border_rect);
+        longButton.setTextColor(getResources().getColor(R.color.colorMainRed));
+        longButton.setText(getResources().getString(R.string.signup));
+    }
+
+    private void initLogInButtonLayout() {
+        logInButton.setBackgroundColor(getResources().getColor(R.color.colorLoginBlue));
+        logInButton.setTextColor(getResources().getColor(R.color.colorWhite));
+
+        signUpButton.setTextColor(getResources().getColor(R.color.colorLoginBlue));
+        signUpButton.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+
+        longButton.setBackgroundResource(R.drawable.blue_border_rect);
+        longButton.setTextColor(getResources().getColor(R.color.colorLoginBlue));
+        longButton.setText(getResources().getString(R.string.login));
+    }
+
+    @OnClick(R.id.signUpButton)
+    public void onClickSignUpButton() {
+        initSignUpButtonLayout();
+    }
+
+    @OnClick(R.id.logInButton)
+    public void onClickLogInButton() {
+        initLogInButtonLayout();
     }
 
     @OnClick(R.id.forgot_password)
